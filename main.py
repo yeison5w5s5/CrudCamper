b05014900f25858060f5287a6e357faf5c235e5d
yeison5w5s5 <104599687+yeison5w5s5@users.noreply.github.com>
2024-01-26 12:57:36 -0500

    manucampers commit 1

---
+++ main.py
@@ -0,0 +1,10 @@
+from modules import camper, trainer, validate
+
+def menu():
+    print("""Sistema de almacenamiento de datos para:
+1- Informacion camper
+2- Infromacion Trainer
+o- Salir""")
+bandera=True
+while (bandera):
+    
