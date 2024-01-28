4e8c7ecf8ed24d955dddeefee3230f94c25bbb0c
yeison5w5s5 <104599687+yeison5w5s5@users.noreply.github.com>
2024-01-28 00:55:07 -0500

    feat: :truck: reemplazo
    lo hhice en otro archivo para probr y lo pase a aqui

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from modules import camper, trainer, validate
 from os import system
+from modulos import camper, trainer
 def menu():
-    print("""Sistema de almacenamiento de datos para:
-\t1- Informacion camper
-\t2- Infromacion Trainer
-\to- Salir""")
-bandera=True
-while (bandera):
+    print("""
+    Este es el menu principal
+        1- Menu Camper
+        2- Menu Trainer
+        3- Salir""")
+Ban=True
+while Ban:
     menu()
-    opc=int(input())
+    opc=int(input("\t"))
     match(opc):
-        case 1:
+        case(1):
             system("clear")
-            camper.menu()
+            camper.mcamper()
+        case(2):
+            system("clear")
+            trainer.mtrainer()
+        case(3):
+            system("clear")
+            print("\t===Fin===")
+            Ban=False
+        case(_):
+            print("otra vez")
---
+++ modulos/camper.py
@@ -0,0 +1,40 @@
+from os import system
+
+def guardar():
+    system("clear")
+    print("guardando")
+def buscar():
+    system("clear")
+    print("Buscando")
+def editar():
+    system("clear")
+    print("Editando")
+def eliminar():
+    system("clear")
+    print("Eliminando")
+
+def mcamper():
+    Ban=True
+    while Ban:
+        print("""
+        Menu Camper
+            1- Guardar camper
+            2- Buscar Camper
+            3- Editar Camper
+            4- Eliminar Camper
+            5- Salir""")
+        opc=int(input("\t"))
+        match(opc):
+            case(1):
+                guardar()
+            case(2):
+                buscar()
+            case(3):
+                editar()
+            case(4):
+                eliminar()
+            case(5):
+                system("clear")
+                Ban=False
+            case (_):
+                print("otra vez")
